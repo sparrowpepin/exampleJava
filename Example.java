d6d51cfd0ec4aa3db5fdb0d907862c794c8ee2dc
Guillaume S-P <gsparrowpepin@putneyschool.org>
2017-01-12 08:22:35 -0500

    added loop

--- Example.java
+++ Example.java
@@ -1,5 +1,9 @@
 class Example {
 	public static void main(String[] args) {
 		System.out.println("Hello World, I will sing you a song");
+		
+		for (int i = 99; i > 0; i--) {
+			System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer! You take one down pass it around " + (i-1) + " bottles of beer on the wall!");
+		}
 	}
 }
